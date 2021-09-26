c726eb31702e863a47445df5d9497594951cbb44
grafit_off <graf.off.it@gmail.com>
2021-09-26 20:46:27 +0300

    Npm cache cleaned

--- src/js/resources/_scriptsTemplates.js
+++ src/js/resources/_scriptsTemplates.js
@@ -5,22 +5,55 @@ const headerMenu = document.querySelector('.header__menu');
 const headerLink = document.querySelectorAll('.header__link');
 const md3 = window.matchMedia("(max-width: 767px)");
 
-function burgerToggle() {
-	headerBurger.classList.toggle('active');
-	headerMenu.classList.toggle('active');
-	body.classList.toggle('lock');
-}
-headerBurger.addEventListener('click', () => {
-	burgerToggle()
-})
-if (md3.matches) {
-	headerLink.forEach((el) => {
-		el.addEventListener('click', () => {
-			setTimeout(() => {
-				burgerToggle()
-			}, 300)
+class Burger {
+	constructor(btn, list) {
+		this.btn = btn;
+		this.list = list;
+		this.opened = false;
+		this.isIphone = isiPhone || isiPad || isiPod;
+		this.btnDisabled = false;
+	}
+
+	open() {
+		this.disableBtn();
+		if (this.isIphone) {
+			disableScroll();
+		} else {
+			body.classList.add('lock')
+		}
+		this.btn.classList.add('burger--active');
+		this.btn.setAttribute('aria-expanded', true);
+		this.list.classList.add('header__nav--active');
+		this.list.setAttribute('aria-hidden', false);
+	}
+
+	close() {
+		this.disableBtn();
+		if (this.isIphone) {
+			enableScroll();
+		} else {
+			body.classList.remove('lock')
+		}
+		this.btn.classList.remove('burger--active');
+		this.btn.setAttribute('aria-expanded', false);
+		this.list.classList.remove('header__nav--active');
+		this.list.setAttribute('aria-hidden', true);
+	}
+
+	toggle() {
+		if (this.btn.classList.contains('burger--active')) {
+			this.close();
+		} else {
+			this.open();
+		}
+	}
+
+	disableBtn() {
+		this.btn.disabled = true;
+		this.list.addEventListener('transitionend', () => {
+			this.btn.disabled = false;
 		})
-	})
+	}
 }
 // -- //
 
